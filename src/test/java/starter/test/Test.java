541d35d9b86da6c529b97d103c15483868df210e
Thùy Chiêu <chieuthuymaijenny@gmail.com>
2022-09-06 16:55:19 +0700

    fix

--- src/test/java/starter/test/Test.java
+++ src/test/java/starter/test/Test.java
@@ -5,6 +5,7 @@ import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
 import net.serenitybdd.screenplay.annotations.CastMember;
 import net.serenitybdd.screenplay.ensure.Ensure;
+import net.serenitybdd.screenplay.questions.Text;
 import net.serenitybdd.screenplay.waits.WaitUntil;
 import net.thucydides.core.annotations.Title;
 import org.junit.jupiter.api.BeforeEach;
@@ -17,7 +18,9 @@ import starter.action.LookForFood;
 
 import java.time.Duration;
 
+import static net.serenitybdd.screenplay.GivenWhenThen.*;
 import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isVisible;
+import static org.hamcrest.CoreMatchers.is;
 
 
 @RunWith(SerenityRunner.class)
@@ -33,9 +36,11 @@ public class Test {
     @org.junit.Test
     @Title("Purchase a package of food")
     public void shoppingCart() {
+        givenThat(user).attemptsTo(NavigateTo.thePerromartPage());
+
+        when(user).attemptsTo(LookForFood.homePage());
         user.attemptsTo(
                 //Search food and add to cart
-                NavigateTo.thePerromartPage(),
                 LookForFood.homePage(),
                 LookForFood.collectionPage("Bronco Salmon"),
                 LookForFood.searchPage("Bronco Jerky Salmon Dog Treat 70g"),
@@ -52,6 +57,9 @@ public class Test {
                 //Verify shipping information
                 Ensure.that(CheckOutShippingPage.LBL_CONTACT).hasText("chieuthuymaijenny@gmail.com"),
                 Ensure.that(CheckOutShippingPage.LBL_SHIP_TO).hasText("Danang" + ", FHome" + ", Singapore " + "520147")
+        );
+        then(user).should(
+                seeThat("", Text.of(CheckOutShippingPage.LBL_CONTACT), is("chieuthuymaijenny@gmail.com"))
         );
     }
 }
